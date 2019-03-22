9f884df853305fbd86ee70caf5f6f6e8d90fbc6f
jegaveerpandian <43804165+jegaveerpandian@users.noreply.github.com>
2019-03-22 12:12:29 +0530

    trial  version 1

---
+++ XLRelease/Samples & Tutorials/task1/bofa corp.groovy
@@ -0,0 +1,25 @@
+// Exported from:        http://e12-PC:5516/#/templates/FolderSamplesAndTutorials-Folder26de3d6994e84e6c80603153287905fc-Released8e48fd9b5404841857093df9a56c7ad/releasefile
+// XL Release version:   8.5.4
+// Date created:         Fri Mar 22 12:12:24 IST 2019
+
+xlr {
+  template('bofa corp') {
+    folder('Samples & Tutorials/task1')
+    description 'task 1'
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2019-03-21T21:30:00+0530')
+    phases {
+      phase('initial task') {
+        color '#0099CC'
+        tasks {
+          manual('trial 1') {
+            
+          }
+          manual('trial 2') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
